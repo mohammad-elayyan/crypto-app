1f4867f11788007c0504ecc289c2df934ee376da
Mohammad El'ayan <Mohammad.El'ayan@Eranovel.local>
2024-09-08 17:31:42 +0300

    upload

---
+++ src/pages/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const Home = () => {
+  return (
+    <main className="w-full h-full flex flex-col first-letter:content-center items-center text-white relative font-nunito">
+      <div className="w-screen h-screen bg-gray-300 fixed -z-10" />
+      <Logo />
+      <Navigation />
+      <Outlet />
+    </main>
+  );
+};
+
+export default Home;
